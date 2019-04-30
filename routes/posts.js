6a8624e6c8be3973e490788fab45623fe863a157
muaz <ahmedmuaz0152@gmail.com>
2019-05-01 00:32:18 +0800

    fixed for get by id, put and delete operation

--- routes/posts.js
+++ routes/posts.js
@@ -1,43 +1,49 @@
-var express = require('express');
-var router = express.Router();
-const posts  = require('../models').Post;
+var express = require('express')
+var router = express.Router()
+const posts = require('../models').Post
+
 /* GET users listing. */
-router.get('/posts', function(req, res, next) {
-  posts.findAll().then( result => {
-      res.json(result)
-      console.log(result)
-      next()
+router.get('/all', function (req, res, next) {
+  posts.findAll().then(result => {
+    res.json(result)
+    console.log(result)
+    next()
   })
-});
-router.get('/post/:id', function(req, res, next) {
-    console.log(req.params.id)
-    posts.findByPk({ where: { id:id }}).then( result => 
+})
+router.get('/post/:id', function (req, res, next) {
+  posts.findOne({ model: posts, where: { id: req.params.id } }).then(result =>
     res.json(result))
-});
-router.post('/', function(req, res, next) {
-    posts.create(req.body).then( result => {
-        res.json(result)
-        console.log(result)
-        next()
-    })
-    .catch(error => {
-        res.status(500)
-        console.log(err)
-    })
-});
-router.put('/post/:id', function(req, res, next) {
-    posts.update(req.body).then( result => {
-        res.json(result)
-        res.status(200)
-        next();
-    });
-router.delete('/post/:id', function(req, res, next) {
-    posts.destroy(req.params.id).then( result => {
-        res.json(result)
-        res.status(200)
-        next();
-    })
-});
+})
+router.post('/', function (req, res, next) {
+  posts.create(req.body).then(result => {
+    res.json(result)
+    console.log(result)
+    next()
+  })
+})
+router.put('/update/:id', function (req, res, next) {
+  const data = {
+    title: req.params.title,
+    post: req.params.post,
+    postedBy: req.params.postedBy
+  }
+  posts.update(data, { where: { id: req.params.id } }).then(result => {
+    res.json(result)
+    res.status(200)
+    next()
+  })
+})
+router.delete('/delete/:id', function (req, res, next) {
+  const id = req.params.id
+  if (!id) {
+    console.log('The id is nowhere to be found. pls try with diiff no now')
+  }
+  console.log(id)
+  posts.destroy({ where: { id: id } }).then(result => {
+    res.status(200)
+    res.json(result)
+    
+  })
 })
 
-module.exports = router;
+module.exports = router
